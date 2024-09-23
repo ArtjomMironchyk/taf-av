6a1ee983d3b9af3721b3cd78411e7fb2ab60a0ca
Artsiom.Mironchyk <artjommironchyk@gmail.com>
2024-09-23 21:13:10 +0300

    Добавил проверку сообщения об ошибке в поле Пароль

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -1,10 +1,11 @@
-package by.av.mironchyk;
+package page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class HomePage {
